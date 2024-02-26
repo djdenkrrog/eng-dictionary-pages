6f260e09e0f8d504a1fcca41e4fb4a9b5ba6ad23
makarov.denys <djdenkrrog@gmail.com>
2024-02-26 23:23:23 +0200

    [Auto]Changed files docs/index.html docs/index.html.gz docs/public/EnglishDictionaryApp.js docs/public/EnglishDictionaryApp.js.gz docs/public/EnglishDictionaryApp_vendor-react.js docs/public/EnglishDictionaryApp_vendor-react.js.gz

--- docs/public/EnglishDictionaryApp_vendor-react.js
+++ docs/public/EnglishDictionaryApp_vendor-react.js
@@ -1,4 +1,4 @@
-/*! EnglishDictionaryApp master (20240226:2231); build: #1.0.0 */
+/*! EnglishDictionaryApp master (20240226:2323); build: #1.0.0 */
 "use strict";(self.webpackChunkeng_dictionary=self.webpackChunkeng_dictionary||[]).push([[56],{1648:(e,n,t)=>{
 /**
  * @license React
